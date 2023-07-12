8f41fdd8b6efca044a848c13058d53efe53d2a53
AliiceK <112477235+AliiceK@users.noreply.github.com>
2023-07-12 20:06:17 +0300

    can now create and update order

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home),
-    path('product/', views.product),
-    path('customer/<str:pk_test>/', views.customer),
+    path('', views.Home, name="home"),
+    path('product/', views.product, name="product"),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+    path('create_order/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
 
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+
+from .forms import OrderForm
 
 # Create your views here.
 
@@ -14,8 +16,8 @@ def Home(request):
 
     total_orders = orders.count()
 
-    delivered = orders.filter('Delivered').count()
-    pending = orders.filter('Pending').count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
 
     context = {'orders': orders, 'customers': customers,
     'total_orders': total_orders, 'delivered': delivered,
@@ -38,3 +40,27 @@ def customer(request, pk_test):
 
     context = {'customer': customer, 'orders': orders, 'order_count': order_count}
     return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance = order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
