ad0c18985631766aca4bdbab7d9ed24256023242
AliiceK <112477235+AliiceK@users.noreply.github.com>
2023-07-18 19:14:56 +0300

    created user form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path("register/", views.registerPage, name="register"),
+    path("login/", views.loginPage, name="login"),
     path('', views.Home, name="home"),
     path('product/', views.product, name="product"),
     path('customer/<str:pk_test>/', views.customer, name="customer"),
