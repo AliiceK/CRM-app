2faba4a2b807d4c816d4fab2e3cc983e5a8c3c5c
AliiceK <112477235+AliiceK@users.noreply.github.com>
2023-07-18 12:43:38 +0300

    added a search ffunctionality to filter based on startdate, notes ...

--- accounts/models.py
+++ accounts/models.py
@@ -48,4 +48,5 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, null = True, on_delete = models.SET_NULL)
     product = models.ForeignKey(Product, null = True, on_delete = models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add = True, null = True)
-    status = models.CharField(max_length = 200, choices = STATUS)
+    status = models.CharField(max_length = 200,null = True, choices = STATUS)
+    note = models.CharField(max_length = 1000,null = True)
